ab0213e9297d0cb983080063a9d8337d4047fa75
akkera102 <akkera102@gmail.com>
2023-04-24 18:46:10 +0900

    初版

---
+++ tut07 スプライト1/obj/sys_arm.d
@@ -0,0 +1,2 @@
+obj/sys_arm.o: lib/sys_arm.c lib/sys_arm.h lib/gba.h lib/common.h \
+ lib/div.h lib/gbfs.h
---
+++ tut07 スプライト1/res/spr2.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(spr2)
+
+//======================================================================
+//
+//	spr2, 8x8@4, 
+//	+ palette 16 entries, not compressed
+//	+ 1 tiles not compressed
+//	Total size: 32 + 32 = 64
+//
+//	Time-stamp: 2023-04-24, 16:41:55
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SPR2_H
+#define GRIT_SPR2_H
+
+#define spr2TilesLen 32
+extern const unsigned char spr2Tiles[32];
+
+#define spr2PalLen 32
+extern const unsigned short spr2Pal[16];
+
+#endif // GRIT_SPR2_H
+
+//}}BLOCK(spr2)
