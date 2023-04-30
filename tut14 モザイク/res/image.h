9c7db16963b9fb30252c61073c14790ec79b4463
akkera102 <akkera102@gmail.com>
2023-04-30 22:03:55 +0900

    初版

---
+++ tut14 モザイク/res/image.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(image)
+
+//======================================================================
+//
+//	image, 240x160@16, 
+//	+ bitmap not compressed
+//	Total size: 76800 = 76800
+//
+//	Time-stamp: 2023-04-30, 21:50:01
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_IMAGE_H
+#define GRIT_IMAGE_H
+
+#define imageBitmapLen 76800
+extern const unsigned short imageBitmap[38400];
+
+#endif // GRIT_IMAGE_H
+
+//}}BLOCK(image)
